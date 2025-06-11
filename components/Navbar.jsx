92a27a06aa9426d7a582cafa7b10b1b567f90484
Canan Seçen Mumcu <canansecenmumcu@gmail.com>
2025-06-11 16:36:54 +0300

    projectsnone

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ import {
 const navLinks = [
   { title: "Home", path: "/" },
   { title: "About", path: "/about" },
-  { title: "Projects", path: "/projects" },
+  /* { title: "Projects", path: "/projects" }, */
   { title: "Contact", path: "/contact" },
 ];
 
